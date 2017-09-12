d03a88374bc92eeeab456b9e47b3111968ef977b
Jincheng Wang <jinchengwone@gmail.com>
2017-09-12 18:26:52 +0800

    Update dataProcess.py

--- Project/Power_Load_Forecasting/dataProcess.py
+++ Project/Power_Load_Forecasting/dataProcess.py
@@ -2,21 +2,38 @@
 
 import csv
 
-filename = "data/Data_Weather.csv"
-tgt = "data/june_weather.csv"
+###### hour_weather
+# filename = "data/Hour_Weather.csv"
+# tgt = "data/hourWeather.csv"
+# file = open(filename,'r')	
+# reader = csv.reader(file)
+# cnt = 0
+# lst = []
+# for row in reader:
+# 	temp = row[0:8]
+# 	temp[0] = cnt%24
+# 	if(temp[5]==""):temp[5] = 0
+# 	lst.append(temp)
+# 	cnt = cnt+1
+
+# csvFile = open(tgt, "wb")
+# writer = csv.writer(csvFile)
+# writer.writerows(lst)
+# csvFile.close()
+
+
+###### june load
+filename = "data/June_Load.csv"
+tgt = "data/juneLoad.csv"
 file = open(filename,'r')	
 reader = csv.reader(file)
 cnt = 0
 lst = []
 for row in reader:
-	if(reader.line_num<=3):
-		temp = row[0:8]
-		temp[0] = cnt%24
-		if(temp[5]==""):temp[5] = 0
-		lst.append(temp)
-		cnt = cnt+1
-	else:
-		break
+	temp = row[0:2]
+	temp[0] = cnt%24
+	lst.append(temp)
+	cnt = cnt+1
 
 csvFile = open(tgt, "wb")
 writer = csv.writer(csvFile)
