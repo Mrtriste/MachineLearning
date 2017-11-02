1413028bd18e5233dad628aa0c9b00f4a2475d0f
MrTriste <jinchengwone@gmail.com>
2017-11-02 22:57:52 +0800

    update SGD

---
+++ Classification/SGD/SGD.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np 
+import matplotlib.pyplot as pyplot
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+
+def get_binary_data():
+	pass
+
+def get_multi_data():
+	pass
+
+def shuffle(X,y):
+	pass
+
+class SGD:
+	def __init__(self):
+		pass
+
+	def _fit_binary(self,X,y):
+		pass
+
+	def _fit_multi(self,X,y):
+		pass
+
+	def fit(self,X,y):
+		pass
+
+	def predict(self,X):
+		pass
+
+def test_binary():
+	pass
+
+def test_multi():
+	pass
+
+if __name__ == '__main__':
+	print 'test'
