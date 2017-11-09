3d8b32e3846d2030fe6079e952581036a4f4b73b
Jincheng Wang <jinchengwone@gmail.com>
2017-11-09 17:42:48 +0800

    Update SGD.py

--- Classification/SGD/SGD.py
+++ Classification/SGD/SGD.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+from sklearn import preprocessing
 import random
 
 def get_binary_data():
-	centers = [[-5,3],[1,-2]]
+	centers = [[-5,3],[1,3]]
 	X,y = make_blobs(centers = centers,n_samples = 1000,random_state=42)
 	trans = [[0.4,0.2],[-0.4,1.2]]
 	X = np.dot(X,trans) + np.random.rand(1000,2)*2.5
 	return X,y
 
 def get_multi_data():
-	centers = [[-5,2],[-1.5,-1],[-1,5]]
+	centers = [[-5,3],[1,3],[-3,-3]]
 	X,y = make_blobs(centers = centers,n_samples = 1000,random_state=42)
 	trans = [[0.4,0.2],[-0.4,1.2]]
 	X = np.dot(X,trans) + np.random.rand(1000,2)*2.5
@@ -27,105 +28,57 @@ def get_image_data():
 
 def shuffle(index):
 	n = index.shape[0]
-	for i in range(n):
-		idx = i+random.randint(1,n-1)
+	for i in range(n-1):
+		idx = random.randint(i+1,n-1)
 		index[[i,idx]] = index[[idx,i]]
 
 ############### begin LossFunction
+#http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.58.7377
+#Section 4
 class LossFunction:
 	def deriv(self,y,p):
-		pass
+		# loss' is deriv about p, that's w*x
+		return 0.0
 		
 class HingeLoss(LossFunction):
 	def deriv(self,y,p):
-		pass
+		# loss = 1 - py if py < 1 else 0
+		# loss' = -y
+		z = p*y
+		if z < 1.0:
+			return -y
+		return 0.0
 
-class SquareLoss(LossFunction):
+class LogisticLoss(LossFunction):
 	def deriv(self,y,p):
-		pass
+		# loss = ln(1+exp(-py))
+		# loss' = (-y)*[exp(-py)/(1+exp(-py))] = (-y)/(1+exp(z))
+		z = y*p
+		if z > 18.0:
+			return -y * np.exp(-z)
+		if z < -18.0:
+			return -y
+		return -y / (1.0 + np.exp(z))
+
+class ModifiedHuberLoss(LossFunction):
+	def deriv(self,y,p):
+		# 1. if py>1, loss = 0; 2. if -1<=py<=1, loss = (1-py)^2; 3.if py<-1,loss = -4py
+		# 1. loss'=0; 2. loss' = 2(1-py)*(-y) 3. loss' = -4y
+		z = p * y
+		if z > 1:
+			return 0.0
+		if z <-1:
+			return -4*y
+		return 2*(1-z)*(-y)
+
+class SquaredHingeLoss(LossFunction):
+	def deriv(self,y,p):
+		# 1. if py>1, loss = 0; 2. if py < 1, loss = (1-py)^2
+		# 1. loss' = 0; 2. loss' = 2(1-py)*(-y)
+		z = p * y
+		if z > 1:
+			return 0.0
+		return 2 * (1 - z) * (-y)
+
 
 ############### end LossFunction
-
-############### begin SGD classification class
-'''
-	support penality: none,l2
-	support learning rate: constant,optimal,PA1,PA2
-	support loss function: hinge,square_loss
-'''
-class SGD:
-	LOSS_FUNCTION = {
-		'hinge':(HingeLoss,),
-		'squareloss':(SquareLoss,)
-	}
-
-	def __init__(self,iter_num=20,loss_type='squareloss',
-				 eta0=0.1,learning_rate='constant',penality='l2'):
-		self.iter_num = iter_num
-		self.loss_type = loss_type
-		self.loss_function = self.LOSS_FUNCTION[loss_type][0]()
-		self.eta0 = eta0
-		self.learning_rate = learning_rate
-		self.penality = penality
-
-	def _fit_binary(self,X,y):
-		n_samples = X.shape[0];n_features = X.shape[1]
-		self.w = np.random.rand(n_features)
-		for it in range(iter_num):
-			shuffle(self.index)
-			for i in range(n_samples):
-				Xi = X[self.index[i]];yi = y[self.next[i]]
-				pass
-
-	def _fit_multi(self,X,y):
-		pass
-
-	def fit(self,X,y):
-		self.class_set = np.unique(y)
-		self.index = np.arange(X.shape[0])
-		if self.class_set.shape[0] == 2:
-			_fit_binary(X,y)
-		else:
-			_fit_multi(X,y)
-
-	def predict(self,X):
-		pass
-
-############### end SGD class
-
-def plot_samples(X,y):
-	x_start = 0
-	if np.mean(X[:,0])==1:
-		x_start += 1
-	c_lst = ['r','g','b','y']
-	y_set = np.unique(y)
-	color = np.array([c_lst[0]]*y.shape[0])
-	for i in range(1,y_set.shape[0]):
-		color[y==y_set[i]] = c_lst[i]
-	plt.figure(figsize=(8,6))
-	plt.scatter(X[:,x_start],X[:,x_start+1],c = color)
-
-def test_binary():
-	X,y = get_binary_data()
-	X = np.column_stack([[1]*X.shape[0],X])
-	X_train,y_train,X_test,y_test = \
-				train_test_split(X,y,test_size=0.2,random_state = np.random.RandomState(42))
-	clf = SGD()
-	clf.fit(X_train,y_train)
-	y_pred = clf.predict(X_test)
-	correct_rate = 1-np.mean(y_test!=y_pred)
-	print 'correct_rate:',correct_rate
-
-	plot_samples(X,y)
-
-
-def test_multi():
-	X,y = get_multi_data()
-	plot_samples(X,y)
-
-def test_image():
-	X,y = get_image_data()
-
-if __name__ == '__main__':
-	# test_binary()
-	# plt.show()
-	b = SGD()
