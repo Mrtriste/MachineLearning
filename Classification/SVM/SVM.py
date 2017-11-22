daff44ce8ca190dc05109b331f213f860a4b7d15
MrTriste <jinchengwone@gmail.com>
2017-11-23 01:06:55 +0800

    update SVM

--- Classification/SVM/SVM.py
+++ Classification/SVM/SVM.py
@@ -5,7 +5,7 @@ from Solver import Solver
 
 ############### SVM class
 class SVM:
-	def __init__(self,kernel_type='Linear',degree=3,gamma='auto',coef0=0.0,epsilon=0.1,C=1.0):
+	def __init__(self,kernel_type='Linear',degree=3,gamma='auto',coef0=0.0,epsilon=0.3,C=0.03):
 		self.param =  Q.SVM_Param(kernel_type,degree,gamma,coef0,epsilon,C)
 
 	def fit(self,X,y):
@@ -14,13 +14,7 @@ class SVM:
 		self.alpha = model.alpha
 		self.b = -model.rho
 		if self.param.kernel_type is 'Linear':
-			m = X.shape[1]; n = X.shape[0]
-			self.w = np.ones(m)
-			for i in range(m):
-				sum_ = 0
-				for j in range(n):
-					sum_ += y[j]*self.alpha[j]*X[j,i]
-				self.w[i] = sum_
+			self.w = np.dot(X.T,self.alpha*y)
 
 	def predict(self,X):
 		pass
