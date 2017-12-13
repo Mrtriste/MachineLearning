b4e410b44edbaf1f26901fc034bb85e40470ed7b
MrTriste <jinchengwone@gmail.com>
2017-12-14 01:21:25 +0800

    update XGBoost.py

--- Classification/Decision Trees/XGBoost/XGBoost.py
+++ Classification/Decision Trees/XGBoost/XGBoost.py
@@ -3,21 +3,22 @@ import numpy as np
 from Booster import *
 
 def softmax(X):
-	exp_X = np.exp(X)
+	X_ = (X.T - np.max(X,axis=1)).T
+	exp_X = np.exp(X_)
 	sum = np.sum(exp_X,axis=1)
 	return np.divide(exp_X.T,sum).T
 
 class XGBoost:
-	def __init__(self,lamda=0.1):
+	def __init__(self,lamda=0.1,min_weight = 1.0,max_iter = 10):
 		self.cfg = {}
-		self.cfg['max_iter'] = 10
+		self.cfg['max_iter'] = max_iter
 		self.cfg['n_class'] = 2
 		self.cfg['lamda'] = lamda
-
+		self.cfg['min_weight'] = min_weight
+ 
 	def fit(self,X,y):
 		self.X = X; self.y = y;
 		n_class = np.unique(y).shape[0]
-		if n_class == 2: n_class = 1
 		self.n_class = n_class
 		self.cfg['n_class'] = n_class
 		n = X.shape[0];m = X.shape[1]
@@ -26,38 +27,46 @@ class XGBoost:
 		self.p = p; self.g = g; self.h = h
 		self.Trees = []
 
-		order = np.argsort(X,axis=1)
-
+		order = np.argsort(X,axis=0)
 		booster = Booster(X,order) 
 
 		for i in range(self.cfg['max_iter']):
+			print 'iter:',i
+			self._predict_raw(X)
+			self._get_gradient()
 			info = {
 				'n_class':n_class,
 				'grad':self.g,
 				'hess':self.h,
-				'lamda':self.cfg['lamda']
+				'lamda':self.cfg['lamda'],
+				'min_weight':self.cfg['min_weight']
 			}
 			booster.train(info)
 			self.Trees.append(booster.get_trees())
 
 	def predict(self,X):
-		pass
+		self._predict_raw(X)
+		sm = softmax(self.p[0:X.shape[0],:])
+		return sm.argmax(axis = 1)
 
 	def _predict_raw(self,X):
 		p = self.p
+		p.fill(0.5)
+		if len(self.Trees) == 0:
+			return
 		for i in range(X.shape[0]):
 			for c in range(self.n_class):
 				# trees: n_class tree
-				p_sum = 0
+				p_sum = p[i,c]
 				for trees in self.Trees:
-					tree = tree[c]
+					tree = trees[c]
 					p_sum += tree.get_score(X[i,:])
 				p[i,c] = p_sum
 
 	def _get_gradient(self):
 		X = self.X; y = self.y
+		g = self.g; h = self.h
 		sm = softmax(self.p)
-		print sm
 		for i in range(0,X.shape[0]):
 			for j in range(0,self.n_class):
 				if j == y[i]: 
@@ -65,5 +74,3 @@ class XGBoost:
 				else: 
 					g[i,j] = sm[i,j]
 				h[i,j] = sm[i,j]*(1-sm[i,j]) #*2.0
-		print g
-		print h
