cc7bae8df016bc7cb13cb2e71f8abcb628fa76fb
MrTriste <jinchengwone@gmail.com>
2017-12-23 00:17:38 +0800

    update GBDT/test

--- Classification/Decision Trees/GBDT/test.py
+++ Classification/Decision Trees/GBDT/test.py
@@ -8,7 +8,7 @@ from sklearn import preprocessing
 from GBDT import *
 
 def get_multi_data():
-	n = 20
+	n = 1000
 	centers = [[-5,3],[1,3],[-3,-3]]
 	X,y = make_blobs(centers = centers,n_samples = n,random_state=42)
 	trans = [[0.4,0.2],[-0.4,1.2]]
@@ -24,28 +24,33 @@ def test_multi():
 	X_train,X_test,y_train,y_test = \
 				train_test_split(X,y,test_size=0.2,random_state = np.random.RandomState(42))
 
-	clf = GBDT(1)
-	# X_train = X_train[0:50,:];y_train = y_train[0:50]
-	# X_train = np.array([[1],[2],[3],[4],[5],[11],[12],[13],[14],[15]])
-	# y_train = np.array([0,0,0,0,0,1,1,1,1,1])
+	eps = [0.01,0.03,0.1,0.3,1]
+	for e in eps:
+		clf = GBDT(11,6,e)
+		clf.fit(X_train,y_train)
+		y_pred = clf.predict(X_test)
+		correct_rate = 1-np.mean(y_test!=y_pred)
+		print 'correct_rate:',correct_rate ,'para:',e
+
+def test_image():
+	X,y = get_image_data()
+	# X = np.column_stack([[1]*X.shape[0],X])
+	X_train,X_test,y_train,y_test = \
+				train_test_split(X,y,test_size=0.2,random_state = np.random.RandomState(42))
+	clf = GBDT()
 	clf.fit(X_train,y_train)
 	y_pred = clf.predict(X_test)
-	# print y_pred
-	# print y_test
 	correct_rate = 1-np.mean(y_test!=y_pred)
-	print 'correct_rate:',correct_rate #,'para:',l,m
+	print 'correct_rate:',correct_rate
 
 if __name__ == '__main__':
-	a = np.array([[1,3,4],[5,6,7],[3,10,6],[4,6,8]],dtype=float)
-	# print softmax(a)
-	# for i in a[:,0]:
-	# 	print i
-	# t = [0 for i in range(10)]
-	# print t
-	# print 1e12
 	# test_multi()
-	a = np.array([[1],[2],[3],[4],[5],[10],[11],[12],[13],[14]])
-	b = np.array([0,0,0,0,0,1,1,1,1,1])
-	clf = GBDT(1)
-	clf.fit(a,b)
+	test_image()
+	# a = np.array([[1],[2],[3],[4],[5],[10],[11],[12],[13],[14]])
+	# # a = np.array([[1],[2],[10],[11]])
+	# # b = np.array([0,0,1,1])
+	# b = np.array([0,0,0,0,0,1,1,1,1,1])
+	# clf = GBDT(6)
+	# clf.fit(a,b)
+
 
