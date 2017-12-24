7cd0fcce362ed1951634a19ef5d6f126905f6f09
MrTriste <jinchengwone@gmail.com>
2017-12-25 02:07:52 +0800

    add KNN

---
+++ Classification/KNN/KNN.py
@@ -0,0 +1,19 @@
+# -*- coding -*-
+
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+import random
+
+class KNN:
+	def __init__(self,k=1):
+		pass
+
+	def fit(self,X,y):
+		pass
+
+	def predict(self,X):
+		pass
+
+if __name__ == '__main__':
+	pass
