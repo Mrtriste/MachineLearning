affc52a8fc47483d9cee7dadc3a88bc968a23c50
MrTriste <jinchengwone@gmail.com>
2018-01-21 23:09:17 +0800

    update test.py

--- Classification/Decision Trees/XGBoost/test.py
+++ Classification/Decision Trees/XGBoost/test.py
@@ -23,29 +23,29 @@ def test_multi():
 	X,y = get_multi_data()
 	X_train,X_test,y_train,y_test = \
 				train_test_split(X,y,test_size=0.2,random_state = np.random.RandomState(42))
-	lamda = [0.01,0.03,0.1,0.3,1,3]
-	# min_w = [0.1,0.3,1,2,3]
+	# lamda = [0.01,0.03,0.1,0.3,1,3]
+	lamda = [10,30,60,100]
+	min_w = [0.1,0.3,1]
 	# lamda = [3]
 	# min_w = [0.1]
 	for l in lamda:
-		# for m in min_w:
-		clf = XGBoost(l,1,6)
-		# X_train = X_train[0:50,:];y_train = y_train[0:50]
-		# X_train = np.array([[1],[2],[3],[4],[5],[11],[12],[13],[14],[15]])
-		# y_train = np.array([0,0,0,0,0,1,1,1,1,1])
-		clf.fit(X_train,y_train)
-		y_pred = clf.predict(X_test)
-		# print y_pred
-		# print y_test
-		correct_rate = 1-np.mean(y_test!=y_pred)
-		print 'correct_rate:',correct_rate,'para:',l,m
+		for m in min_w:
+			clf = XGBoost(l,m,13)
+			# X_train = np.array([[1],[2],[3],[4],[5],[11],[12],[13],[14],[15]])
+			# y_train = np.array([0,0,0,0,0,1,1,1,1,1])
+			clf.fit(X_train,y_train)
+			y_pred = clf.predict(X_test)
+			# print y_pred
+			# print y_test
+			correct_rate = 1-np.mean(y_test!=y_pred)
+			print 'correct_rate:',correct_rate,'para:',l,m
 
 def test_image():
 	X,y = get_image_data()
 	# X = np.column_stack([[1]*X.shape[0],X])
 	X_train,X_test,y_train,y_test = \
 				train_test_split(X,y,test_size=0.2,random_state = np.random.RandomState(42))
-	clf = XGBoost(0.1,1)
+	clf = XGBoost(10,1)
 	clf.fit(X_train,y_train)
 	y_pred = clf.predict(X_test)
 	correct_rate = 1-np.mean(y_test!=y_pred)
@@ -54,4 +54,5 @@ def test_image():
 
 if __name__=='__main__':
 	test_multi()
+	# not work on image
 	# test_image()
